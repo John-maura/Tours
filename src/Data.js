cd8d8c9a8a7f649724989a3b20893dbc23d1267c
John-maura <johntemwaura992@gmail.com>
2021-06-20 08:54:51 +0300

    added  loading,Tour and Tours components then modified App component

---
+++ src/Data.js
@@ -0,0 +1,39 @@
+const Data = [
+    {
+        id: 1,
+        name: "Best of 7 days in Maasai mara",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil esse corrupti, dicta, et suscipit error aperiam quo a beatae ipsam cum, rerum cumque. Dolorum mollitia, nihil culpa cupiditate quaerat doloribus ducimus tempora ex. Totam numquam tempora sunt quam exercitationem ipsa. Laborum quidem voluptatum nulla laboriosam culpa amet suscipit, exercitationem fugit fugiat earum, eos, voluptas quia nihil distinctio repellendus totam tempore?",
+        image: "https://hdjjjnfjsdhfjgk.jpeg",
+        price: "45650"
+    },
+     {
+        id: 2,
+        name: "Best of 5 days Tsavo",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil esse corrupti, dicta, et suscipit error aperiam quo a beatae ipsam cum, rerum cumque. Dolorum mollitia, nihil culpa cupiditate quaerat doloribus ducimus tempora ex. Totam numquam tempora sunt quam exercitationem ipsa. Laborum quidem voluptatum nulla laboriosam culpa amet suscipit, exercitationem fugit fugiat earum, eos, voluptas quia nihil distinctio repellendus totam tempore?",
+        image: "https://hdjjjnfjsdhfjgk.jpeg",
+        price: "32000"
+    },
+     {
+        id: 3,
+        name: "Best of 6 days in Amboseli",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil esse corrupti, dicta, et suscipit error aperiam quo a beatae ipsam cum, rerum cumque. Dolorum mollitia, nihil culpa cupiditate quaerat doloribus ducimus tempora ex. Totam numquam tempora sunt quam exercitationem ipsa. Laborum quidem voluptatum nulla laboriosam culpa amet suscipit, exercitationem fugit fugiat earum, eos, voluptas quia nihil distinctio repellendus totam tempore?",
+        image: "https://hdjjjnfjsdhfjgk.jpeg",
+        price: "38600"
+    },
+     {
+        id: 4,
+        name: "Best of 3 days in Mt kenya",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil esse corrupti, dicta, et suscipit error aperiam quo a beatae ipsam cum, rerum cumque. Dolorum mollitia, nihil culpa cupiditate quaerat doloribus ducimus tempora ex. Totam numquam tempora sunt quam exercitationem ipsa. Laborum quidem voluptatum nulla laboriosam culpa amet suscipit, exercitationem fugit fugiat earum, eos, voluptas quia nihil distinctio repellendus totam tempore?",
+        image: "https://hdjjjnfjsdhfjgk.jpeg",
+        price: "23100"
+    },
+     {
+        id: 5,
+        name: "Best of 8 days in kilifi",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil esse corrupti, dicta, et suscipit error aperiam quo a beatae ipsam cum, rerum cumque. Dolorum mollitia, nihil culpa cupiditate quaerat doloribus ducimus tempora ex. Totam numquam tempora sunt quam exercitationem ipsa. Laborum quidem voluptatum nulla laboriosam culpa amet suscipit, exercitationem fugit fugiat earum, eos, voluptas quia nihil distinctio repellendus totam tempore?",
+        image: "https://hdjjjnfjsdhfjgk.jpeg",
+        price: "60650"
+    }
+]
+
+export default Data;
